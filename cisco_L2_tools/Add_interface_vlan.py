ee9316fa21c3f4ff6837a06b9b35435d6258e1a6
Israel Alves <israel.alves@gmail.com>
2018-10-16 08:23:31 -0300

    Minor updates on Add_interface_vlan.py

--- cisco_L2_tools/Add_interface_vlan.py
+++ cisco_L2_tools/Add_interface_vlan.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
-""" Add_interface_vlan.py version 0.1 """
-""" Retrieve version from a Cisco IOS device """
-""" By Israel Alves dos Santos Filho - 10/08/2018 """
+""" Add_interface_vlan.py version 0.2 """
+""" Associate a VLAN to physical interface on Cisco IOS device """
+""" By Israel Alves dos Santos Filho - 10/16/2018 """
 
 from netmiko import ConnectHandler
 
@@ -11,11 +11,13 @@ username = input('Username: ')
 password = input('Password: ')
 platform = 'cisco_ios'
 host = '10.0.0.1'
+print ('\n)
 
 """ Interface info & Vlan assigment """
 print ('Interface number and VLAN assigment')
 interface = input('Interface: ')
-vlan	= input('VLAN assigment: ')
+vlan = input('VLAN assigment: ')
+print ('\n)
 
 device = ConnectHandler(device_type=platform, ip=host, username=username, password=password)
 
@@ -25,6 +27,10 @@ command_interface = 'interface ' + interface
 command_vlan = 'switchport access vlan ' + str(vlan)
 commands = [command_interface, 'switchport mode access', command_vlan]
 
+""" VLAN Assigment validation  """
+
+print ('\n\n')
+print(>>>>>>>>>>OUTPUT<<<<<<<<<<)
 output = device.config_mode()
 print(output)
 output = device.send_config_set(commands)
@@ -32,5 +38,4 @@ print(output)
 output = device.exit_config_mode()
 print(output)
 device.disconnect()
-
 print ('\n\n')
